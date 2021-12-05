3b2265796f990440a335afe29dd8d1bb96c08851
Jodie Le <jodietranle@gmail.com>
2021-12-05 00:19:29 -0800

    Testing adding my php files

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Quicksand:400,700">
+</head>
+
+<body class="home">
+	<header>
+		<nav class="home__nav">
+			<ul class="home__nav-list nav-list">
+				<li class="home__nav-items nav-item"><a href="index.php" class="home__nav-link nav-link">Tech Together University - Digital Journal</a></li>
+				<li class="home__nav-items nav-item"><a href="login.php" class="home__nav-link nav-link nav-login">Log In</a></li>
+			</ul>
+		</nav>
+	</header>
